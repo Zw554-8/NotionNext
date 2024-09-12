a48d12e39c31b946d5cfa0cbed1d3fc741d8971a
Zw554-8 <jiehuangjiang@gmail.com>
2024-09-12 14:21:47 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -21,9 +21,9 @@ const CONFIG = {
   HEO_HERO_TITLE_1: '分享编程',
   HEO_HERO_TITLE_2: '与思维认知',
   HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_4: '解放双手',
+  HEO_HERO_TITLE_5: '公众号爆文机器人 轻松矩阵文章',
+  HEO_HERO_TITLE_LINK: 'https://mp.weixin.qq.com/s/7mVurPpHihYAlaTczn7HSg',
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
